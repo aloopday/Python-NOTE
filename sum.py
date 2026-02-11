33892e9d31745bb1c7312801705d993dcddb4609
aloopday <aloopday@gmail.com>
2026-02-12 07:47:48 +0800

    notice extend the python3 in vs

---
+++ sum.py
@@ -0,0 +1,11 @@
+x = 5
+y ="Charles"
+print(x)
+print(y)
+
+# Variables do not need to be declared with any 
+# particular type, and even change type after they have been set.
+
+a = 3 # x is of type int
+a ="sally" # a is now of type str
+print(a) 
