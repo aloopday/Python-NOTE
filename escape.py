c69d0a98682f68a66c06c976d41fa36c2074854c
aloopday <aloopday@gmail.com>
2026-03-01 16:08:38 +0800

    add escape.py format

--- escape.py
+++ escape.py
@@ -1,7 +1,15 @@
 text ="We are the people \"charles\" from the \'hongkong\'."
 a="This will insert a \\ in the line."
 b="This is a \n new line."
-c=""
+c="Hello\tworld!"
+d="Hello \bBeijing"
+# A backslash followed by  three integers will result in a octal value:
+e="\110\145\154\154\157"
+f="\x48\x65\x6c\x6c\x6f"
 print(text)
 print(a)
 print(b)
+print(c)
+print(d)
+print(e)
+print(f)
